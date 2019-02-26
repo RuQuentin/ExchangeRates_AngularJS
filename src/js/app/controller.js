a21caadc4416bd62c49642a184a7366af74ec730
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2019-02-26 22:11:49 +0300

    features/angularJsApp,
    * try to create an object *deal* with properties:
    ccyToSell, ccyToBuy, crossRate
    
    not done
    decided to return to previous model

--- src/js/app/controller.js
+++ src/js/app/controller.js
@@ -1,42 +1,38 @@
+/* eslint-disable max-len */
 /* eslint-disable func-names */
 (() => {
   const currencyConverterApp = angular.module('currencyConverterApp');
 
-  currencyConverterApp.controller('currencyController', ['$scope', 'commissions', 'currencies', 'currencyService', function ($scope, commissions, currencies, currencyService) {
+  currencyConverterApp.controller('currencyController', ['$scope', 'commissions', 'currencies', 'currencyService', 'deal', function ($scope, commissions, currencies, currencyService, deal) {
     $scope.currencies = currencies;
     $scope.currencyToSell = {};
     $scope.currencyToBuy = {};
     $scope.crossPrice = null;
+
+
+    $scope.deal = deal;
 
     $scope.commissions = commissions;
 
     currencyService.updatePrices();
 
     $scope.calcCrossPrice = () => {
-      $scope.crossPrice = currencyService.calcRate($scope.currencyToSell, $scope.currencyToBuy);
+      $scope.crossPrice = currencyService.calcRate();
     };
 
-    $scope.setActive = (currency) => {
-      $scope.currencyActive = currency;
-    };
+    // $scope.setActive = (currency) => {
+    //   $scope.currencyActive = currency;
+    // };
 
-    $scope.$watchGroup(['sumToPay', 'sumToReceive', 'currencyToSell', 'currencyToBuy', 'commission'], () => {
-      if ($scope.currencyActive === $scope.currencyToSell) {
-        $scope.sumToReceive = $scope.sumToPay * $scope.crossPrice * (1 - $scope.commission / 100);
-        $scope.sumToReceive = +$scope.sumToReceive.toFixed(2);
-      } else {
-        $scope.sumToPay = $scope.sumToReceive / $scope.crossPrice / (1 - $scope.commission / 100);
-        $scope.sumToPay = +$scope.sumToPay.toFixed(2);
-      }
-    });
+    // $scope.$watchGroup(['sumToPay', 'sumToReceive', 'currencyToSell', 'currencyToBuy', 'commission'], currencyService.updateSums(sumToPay, sumToReceive, currencyToSell, currencyToBuy, commission));
 
-    $scope.swapCurrencies = () => {
-      [$scope.currencyToSell, $scope.currencyToBuy] = [$scope.currencyToBuy, $scope.currencyToSell];
-    };
+    // $scope.swapCurrencies = () => {
+    //   [$scope.currencyToSell, $scope.currencyToBuy] = [$scope.currencyToBuy, $scope.currencyToSell];
+    // };
 
-    $scope.clearSums = () => {
-      $scope.sumToReceive = null;
-      $scope.sumToPay = null;
-    };
+    // $scope.clearSums = () => {
+    //   $scope.sumToReceive = null;
+    //   $scope.sumToPay = null;
+    // };
   }]);
 })();
--- src/js/app/services.js
+++ src/js/app/services.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+/* eslint-disable no-param-reassign */
 /* eslint-disable func-names */
 /* eslint-disable implicit-arrow-linebreak */
 (() => {
@@ -11,7 +13,19 @@
     ccy: 'UAH', base_ccy: 'UAH', buy: 1, sale: 1,
   }]);
 
-  currencyConverterApp.service('currencyService', ['$http', 'urlPrivate', 'currencies', function ($http, urlPrivate, currencies) {
+  currencyConverterApp.value('deal', [{
+    ccyToExchange: {
+      name: null,
+      sum: null,
+    },
+    ccyToReceipt: {
+      name: null,
+      sum: null,
+    },
+    crossRate: null,
+  }]);
+
+  currencyConverterApp.service('currencyService', ['$http', 'urlPrivate', 'currencies', 'deal', function ($http, urlPrivate, currencies, deal) {
     this.updatePrices = () => {
       $http({
         method: 'GET',
@@ -23,29 +37,55 @@
     };
 
 
-    this.calcRate = (currencyToSell, currencyToBuy) => {
-      let ccyToSell = currencyToSell;
-      let ccyToBuy = currencyToBuy;
+    this.calcRate = () => {
+      // eslint-disable-next-line prefer-const
+      let ccyToSell = deal.ccyToExchange.name;
+      // eslint-disable-next-line prefer-const
+      let ccyToBuy = deal.ToReceipt.name;
 
-      let crossPrice = 1;
+      if (!ccyToSell || !ccyToBuy) return;
 
-      if (!ccyToSell || !ccyToBuy) return crossPrice;
-
-      if (ccyToSell.base_ccy === ccyToBuy.ccy) {
-        crossPrice = +ccyToSell.buy;
-        return crossPrice;
+      if (currencies[ccyToSell].base_ccy === ccyToBuy) {
+        deal.crossRate = +currencies[ccyToSell].buy;
+        // return;
       }
 
-      while (ccyToSell.ccy !== ccyToBuy.ccy) {
-        crossPrice = crossPrice * +ccyToSell.buy / ccyToBuy.sale;
+      // if (currencies[ccyToSell].base_ccy)
 
-        // eslint-disable-next-line no-loop-func
-        ccyToSell = currencies.find(item => item.ccy === ccyToSell.base_ccy);
-        // eslint-disable-next-line no-loop-func
-        ccyToBuy = currencies.find(item => item.ccy === ccyToBuy.base_ccy);
-      }
 
-      return crossPrice;
+      // let ccyToSell = currencyToSell;
+      // let ccyToBuy = currencyToBuy;
+
+      // let crossPrice = 1;
+
+      // if (!ccyToSell || !ccyToBuy) return crossPrice;
+
+      // if (ccyToSell.base_ccy === ccyToBuy.ccy) {
+      //   crossPrice = +ccyToSell.buy;
+      //   return crossPrice;
+      // }
+
+      // while (ccyToSell.ccy !== ccyToBuy.ccy) {
+      //   crossPrice = crossPrice * +ccyToSell.buy / ccyToBuy.sale;
+
+      //   // eslint-disable-next-line no-loop-func
+      //   ccyToSell = currencies.find(item => item.ccy === ccyToSell.base_ccy);
+      //   // eslint-disable-next-line no-loop-func
+      //   ccyToBuy = currencies.find(item => item.ccy === ccyToBuy.base_ccy);
+      // }
+
+      // return crossPrice;
     };
+
+
+    // this.updateSums = () => {
+    //   if ($scope.currencyActive === $scope.currencyToSell) {
+    //     $scope.sumToReceive = $scope.sumToPay * $scope.crossPrice * (1 - $scope.commission / 100);
+    //     $scope.sumToReceive = +$scope.sumToReceive.toFixed(2);
+    //   } else {
+    //     $scope.sumToPay = $scope.sumToReceive / $scope.crossPrice / (1 - $scope.commission / 100);
+    //     $scope.sumToPay = +$scope.sumToPay.toFixed(2);
+    //   }
+    // };
   }]);
 })();
