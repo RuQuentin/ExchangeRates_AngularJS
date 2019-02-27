a745e42e50c3455a4aa15795270140409cc4f654
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2019-02-27 14:41:46 +0300

    features/angularJsApp,
    * remove BUILD folder
    * change service to provider

--- src/js/app/services.js
+++ src/js/app/services.js
@@ -38,65 +38,77 @@
   });
 
 
-  currencyConverterApp.service('currencyService', ['$http', 'urlPrivate', 'currencies', 'deal', function ($http, urlPrivate, currencies, deal) {
-    this.updatePrices = () => {
-      $http({
-        method: 'GET',
-        url: urlPrivate,
-      })
-        .then((response) => {
-          response.data.forEach((item) => {
-            currencies[item.ccy] = item;
-          });
-        });
+  currencyConverterApp.provider('currencyService', function () {
+    // eslint-disable-next-line no-unused-vars
+    let API = '';
+
+    this.setAPI = (apiUrl) => {
+      API = apiUrl;
     };
 
+    this.$get = ['$http', 'urlPrivate', 'currencies', 'deal', function ($http, urlPrivate, currencies, deal) {
+      return {
+        updatePrices: () => {
+          $http({
+            method: 'GET',
+            url: urlPrivate,
+          })
+            .then((response) => {
+              response.data.forEach((item) => {
+                currencies[item.ccy] = item;
+              });
+            });
+        },
 
-    this.calcRate = () => {
-      let ccyToSell = deal.ccyToExchange.name;
-      let ccyToBuy = deal.ccyToReceipt.name;
-      deal.crossRate = 1;
 
-      const convertTowards = () => {
-        deal.crossRate = deal.crossRate * +currencies[ccyToSell].buy / +currencies[ccyToBuy].sale;
+        calcRate: () => {
+          let ccyToSell = deal.ccyToExchange.name;
+          let ccyToBuy = deal.ccyToReceipt.name;
+          deal.crossRate = 1;
+
+          const convertTowards = () => {
+            // eslint-disable-next-line max-len
+            deal.crossRate = deal.crossRate * +currencies[ccyToSell].buy / +currencies[ccyToBuy].sale;
+          };
+
+          if (!ccyToSell || !ccyToBuy) return;
+
+          if (currencies[ccyToSell].base_ccy === ccyToBuy) {
+            deal.crossRate = +currencies[ccyToSell].buy;
+            return;
+          }
+
+          convertTowards();
+
+          ccyToSell = currencies[ccyToSell].base_ccy;
+          ccyToBuy = currencies[ccyToBuy].base_ccy;
+
+          if (ccyToSell !== ccyToBuy) convertTowards();
+        },
+
+
+        swapCurrencies: () => {
+          [deal.ccyToExchange, deal.ccyToReceipt] = [deal.ccyToReceipt, deal.ccyToExchange];
+        },
+
+
+        setActiveCurrency: (currency) => {
+          deal.active = currency;
+        },
+
+
+        updateSums: () => {
+          const { ccyToReceipt, ccyToExchange } = deal;
+
+          if (deal.active === deal.ccyToExchange) {
+            ccyToReceipt.sum = ccyToExchange.sum * deal.crossRate * (1 - deal.commission / 100);
+            ccyToReceipt.sum = +ccyToReceipt.sum.toFixed(2);
+          } else {
+            ccyToExchange.sum = ccyToReceipt.sum / deal.crossRate / (1 - deal.commission / 100);
+            ccyToExchange.sum = +ccyToExchange.sum.toFixed(2);
+          }
+        },
       };
-
-      if (!ccyToSell || !ccyToBuy) return;
-
-      if (currencies[ccyToSell].base_ccy === ccyToBuy) {
-        deal.crossRate = +currencies[ccyToSell].buy;
-        return;
-      }
-
-      convertTowards();
-
-      ccyToSell = currencies[ccyToSell].base_ccy;
-      ccyToBuy = currencies[ccyToBuy].base_ccy;
-
-      if (ccyToSell !== ccyToBuy) convertTowards();
-    };
-
-
-    this.swapCurrencies = () => {
-      [deal.ccyToExchange, deal.ccyToReceipt] = [deal.ccyToReceipt, deal.ccyToExchange];
-    };
-
-
-    this.setActiveCurrency = (currency) => {
-      deal.active = currency;
-    };
-
-
-    this.updateSums = () => {
-      const { ccyToReceipt, ccyToExchange } = deal;
-
-      if (deal.active === deal.ccyToExchange) {
-        ccyToReceipt.sum = ccyToExchange.sum * deal.crossRate * (1 - deal.commission / 100);
-        ccyToReceipt.sum = +ccyToReceipt.sum.toFixed(2);
-      } else {
-        ccyToExchange.sum = ccyToReceipt.sum / deal.crossRate / (1 - deal.commission / 100);
-        ccyToExchange.sum = +ccyToExchange.sum.toFixed(2);
-      }
-    };
-  }]);
+    }];
+  });
 }());
